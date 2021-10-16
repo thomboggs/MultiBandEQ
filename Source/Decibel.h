bf433ce39292d6577a4e964b43741bf660bbe9f4
Thomas Boggs <thomboggs@gmail.com>
2021-10-16 10:57:51 -0400

    Cleaned up decibel.h

--- Source/Decibel.h
+++ Source/Decibel.h
@@ -40,6 +40,7 @@ struct Decibel
     ~Decibel() {};
     
     // Math Operators
+    // LHS
     Decibel& operator+=(const Decibel& other)
     {
         this->gainInDb = this->gainInDb + other.gainInDb;
@@ -64,7 +65,7 @@ struct Decibel
         return *this;
     }
     
-    
+    // RHS
     friend Decibel operator+ (const Decibel& lhs, const Decibel& rhs)
     {
         return Decibel(lhs.gainInDb + rhs.gainInDb);
@@ -143,86 +144,3 @@ struct Decibel
 private:
     FloatType gainInDb {0.f};
 };
-
-//=========================================================================================
-//template <typename FloatType>
-//Decibel<FloatType> operator+ (const Decibel<FloatType>& lhs, const Decibel<FloatType>& rhs)
-//{
-//    return Decibel(lhs.gainInDb + rhs.gainInDb);
-//}
-
-//template <typename FloatType>
-//Decibel<FloatType> operator- (const Decibel<FloatType>& lhs, const Decibel<FloatType>& rhs)
-//{
-//    return Decibel(lhs.gainInDb - rhs.gainInDb);
-//}
-
-//template <typename FloatType>
-//Decibel<FloatType> operator* (const Decibel<FloatType>& lhs, const Decibel<FloatType>& rhs)
-//{
-//    return Decibel(lhs.gainInDb * rhs.gainInDb);
-//}
-
-//template <typename FloatType>
-//Decibel<FloatType> operator/ (const Decibel<FloatType>& lhs, const Decibel<FloatType>& rhs)
-//{
-//    return Decibel(lhs.gainInDb / rhs.gainInDb);
-//}
-
-//friend bool operator==(const Decibel& lhs, const Decibel& rhs);
-//
-//friend bool operator!=(const Decibel& lhs, const Decibel& rhs);
-//
-//friend bool operator<(const Decibel& lhs, const Decibel& rhs);
-//
-//friend bool operator>(const Decibel& lhs, const Decibel& rhs);
-//
-//friend bool operator<=(const Decibel& lhs, const Decibel& rhs);
-//
-//friend bool operator>=(const Decibel& lhs, const Decibel& rhs);
-
-
-/*
-Implement a Decibel<FloatType> class
-
-the purpose of this class is so you don't have to remember to convert decibels to gain units
-
-It is essentially a wrapper around a FloatType member variable, which is private
-
-========Implement:
-
-DONE Rule of 3
-
-DONE math operators for when this object is on the left side of the +=, -=, etc
-
-DONE math operators for when this object is on the right side of the =: auto v = gain1 + gain2;   These should be friend functions
-
-DONE comparison operators.  these should be friend functions
-
-DONE FloatType getGain() const
-
-DONE FloatType getDb() const
-
-DONE setGain(FloatType g)
-
-DONE setDb(FloatType db)
-
-======== 2 constructors:
-
-DONE default constructor
-
-DONE a constructor that takes a FloatType parameter to initialize your member variable.
-
- 
- 
- 
- 
- 
- 
-Why is this class being created:
-
-This class will be used in your FilterParameters class.
-
-It will also be used in the FilterLink class, as the template type for a juce::SmoothedValue<>  The use within SmoothedValue is why you must implement math operators and comparison operators.  The SmoothedValue instance will handle smoothing changes to the filter band’s gain setting.
-
-*/
